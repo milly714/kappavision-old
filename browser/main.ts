ba27c0eb9f03140bfd9faac6bd6061d5c736939e
Pwootage <pwootage@gmail.com>
2015-02-28 18:06:53 -0700

    Modules are pretty good (not sure why it thinks the entire thing changed)

--- browser/main.ts
+++ browser/main.ts
@@ -20,65 +20,65 @@ var mainWindow:any = null;
 //Useful datas
 var dataPath = app.getPath("appData") + "/.kappavision/";
 try {
-  fs.mkdirSync(dataPath);
+    fs.mkdirSync(dataPath);
 } catch (e) {
-  //Don't care!
+    //Don't care!
 }
 
-var twitch = new Twitch(dataPath, () => mainWindow.loadUrl('http://127.0.0.1:15257/index.html'));
+var twitchSession = new twitch.Twitch(dataPath, () => mainWindow.loadUrl('http://127.0.0.1:15257/index.html'));
 
 var express:any = require('express');
 var expSrv = express();
 
 expSrv.get('/test', function (req, res) {
-  res.send(dataPath);
+    res.send(dataPath);
 });
 
 expSrv.get('/twitchlogin.html', function (req, res) {
-  res.sendFile('twitchloginexternal.html', {root: './client'});
+    res.sendFile('twitchloginexternal.html', {root: './client'});
 });
 
 expSrv.get('/token', function(req, res) {
-  res.send("");
-  twitch.login(req.query.access_token);
+    res.send("");
+    twitchSession.login(req.query.access_token);
 });
 
 expSrv.get('/index.html', function (req, res) {
-  if (twitch.loggedIn) {
-    res.sendFile('index.html', {root: './client'});
-  } else {
-    res.sendFile('twitchlogin.html', {root: './client'});
-    require('open')(twitch.loginUrl());
-    //twitch.login("");
-  }
+    if (twitchSession.loggedIn) {
+        res.sendFile('index.html', {root: './client'});
+    } else {
+        res.sendFile('twitchlogin.html', {root: './client'});
+        require('open')(twitchSession.loginUrl());
+        //twitch.login("");
+    }
 });
 
 expSrv.use('/', express.static('./client'));
 
 app.on('window-all-closed', function () {
-  if (process.platform != 'darwin')
-    app.quit();
+    if (process.platform != 'darwin')
+        app.quit();
 });
 
 
 app.on('ready', function () {
-  var server = expSrv.listen(15257, function () {
-    mainWindow = new BrowserWindow({width: 950, height: 550, frame: false});
+    var server = expSrv.listen(15257, function () {
+        mainWindow = new BrowserWindow({width: 950, height: 550, frame: false});
 
-    //var index = 'file://' + __dirname + '/client/index.html';
-    //this is dumb, there's gotta be a better way
-    var index = 'http://127.0.0.1:15257/index.html';
-    console.log('Loading file ' + index);
-    mainWindow.loadUrl(index);
+        //var index = 'file://' + __dirname + '/client/index.html';
+        //this is dumb, there's gotta be a better way
+        var index = 'http://127.0.0.1:15257/index.html';
+        console.log('Loading file ' + index);
+        mainWindow.loadUrl(index);
 
-    mainWindow.on('closed', function () {
-      // Left this comment from the example code for maybe info later
-      // Dereference the window object, usually you would store windows
-      // in an array if your app supports multi windows, this is the time
-      // when you should delete the corresponding element.
-      mainWindow = null;
+        mainWindow.on('closed', function () {
+            // Left this comment from the example code for maybe info later
+            // Dereference the window object, usually you would store windows
+            // in an array if your app supports multi windows, this is the time
+            // when you should delete the corresponding element.
+            mainWindow = null;
+        });
+
+
     });
-
-
-  });
 });
